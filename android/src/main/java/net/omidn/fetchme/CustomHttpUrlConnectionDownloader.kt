0e6336a6798e4a55850baed28478924d9856033a
zhangredli <zhangredli@163.com>
2025-08-17 17:51:10 +0800

    fix - 优化下载线程数

--- android/src/main/java/net/omidn/fetchme/CustomHttpUrlConnectionDownloader.kt
+++ android/src/main/java/net/omidn/fetchme/CustomHttpUrlConnectionDownloader.kt
@@ -2,23 +2,22 @@ package net.omidn.fetchme
 
 import com.tonyodev.fetch2.HttpUrlConnectionDownloader
 import com.tonyodev.fetch2core.Downloader
-import java.lang.Math.ceil
-import kotlin.math.ceil
+import io.flutter.Log
 
 class CustomHttpUrlConnectionDownloader(fileDownloaderType: Downloader.FileDownloaderType) :
     HttpUrlConnectionDownloader(fileDownloaderType) {
 
     override fun getFileSlicingCount(request: Downloader.ServerRequest, contentLength: Long): Int? {
-        val fileSizeInMb = contentLength.toFloat() / 1024F * 1024F
-        val fileSizeInGb = contentLength.toFloat() / 1024F * 1024F * 1024F
+        val fileSizeInMb = contentLength.toFloat() / (1024F * 1024F)
+        val fileSizeInGb = contentLength.toFloat() / (1024F * 1024F * 1024F)
         var slices = when {
-            fileSizeInGb >= 1F -> {
-                ceil(fileSizeInGb * 8).toInt()
-            }
-            fileSizeInMb >= 512F -> 8
-            fileSizeInMb >= 1F -> 4
-            else -> 2
+            fileSizeInGb >= 2F -> 8
+            fileSizeInGb >= 1F -> 6
+            fileSizeInMb >= 512F -> 4
+            fileSizeInMb >= 64F -> 2
+            else -> 1
         }
-        return if (slices > 32) { 32 } else { slices };
+        Log.d("Fetchme", "files slices: $slices")
+        return if (slices > 8) { 8 } else { slices };
     }
 }
